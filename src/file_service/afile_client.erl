cc28720490c9968d32b59ece8ad1a53a8eba9fa1
Sexy-Tunan <brucetutan@gmail.com>
2025-10-15 09:46:31 +0800

    提交erlang多核与单核性能对比测试代码

--- src/file_service/afile_client.erl
+++ src/file_service/afile_client.erl
@@ -12,7 +12,6 @@ get_file(Server, File_Name) ->
     Server ! {self(), {get_file, File_Name}},
     receive
         {Server, Content} ->
-            file:read_file(Filename)
             Content
     end. 
 
